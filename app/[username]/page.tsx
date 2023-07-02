886c9425898ab2a80fcdc0dafe01dd9b565fbb54
Andrew Morrison <andrew.morrisoon@gmail.com>
2023-07-02 21:35:37 +0100

    sosig

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,7 +1,57 @@
-export default function Page() {
+'use client'
+
+import { Form, useForm } from "@/components/form/form";
+import { CheckIcon, TextArea } from "@/components/form/inputs";
+import { z } from "zod";
+import { SubmitHandler } from "react-hook-form";
+import { generateImage } from "@/lib/openai";
+import { useState } from "react";
+import { ImagesResponseDataInner } from "openai";
+
+interface QueryForm {
+  query: string;
+}
+
+const formSchema = z.object({
+  query: z.string().min(1).max(100),
+})
+
+
+export default function Page({ params }: { params: { username: string } }) {
+  const form = useForm({ schema: formSchema })
+  const [images, setImages] = useState<ImagesResponseDataInner[]>([])
+
+
+  const onSubmit: SubmitHandler<QueryForm> = async (data) => {
+    try {
+      console.log(data.query)
+
+      const images = await generateImage(data.query)
+      setImages(images.data)
+
+
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
-    <div className="flex justify-center">
-      username
+    <div className="flex flex-col items-center">
+      <h1 className="label text-3xl">{params.username}</h1>
+      <div>
+        <Form form={form} onSubmit={onSubmit}>
+          <TextArea label="query" {...form.register('query')} />
+          <CheckIcon label="human" />
+          <button className="btn btn-secondary mt-4" type="submit">Submit</button>
+        </Form>
+      </div>
+      <div className="flex w-full">
+        {images.map((image, index) => (
+          <div className=" relative w-full">
+            <img src={image.url!} width={1024} height={1024} alt={`Image ${index}`} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- components/form/inputs.tsx
+++ components/form/inputs.tsx
@@ -5,6 +5,14 @@ interface InputProps extends ComponentProps<"input"> {
   label: string;
 }
 
+interface TextAreaProps extends ComponentProps<"textarea"> {
+  label: string;
+}
+
+interface CheckIconProps extends ComponentProps<"input"> {
+  label: string;
+}
+
 export const Input = forwardRef<HTMLInputElement, InputProps>(({ label, type = 'text', ...props }, ref) => (
   <div >
     <label className="label">{label}</label>
@@ -14,4 +22,23 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(({ label, type = '
 ))
 
 Input.displayName = 'Input'
+
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({ label, ...props }, ref) => (
+  <div >
+    <label className="label">{label}</label>
+    <textarea className="textarea textarea-bordered " ref={ref} {...props} />
+    <FieldError name={props.name} />
+  </div>
+))
+
+export const CheckIcon = forwardRef<HTMLInputElement, CheckIconProps>(({ label, ...props }, ref) => (
+  <div>
+    <input type="checkbox" className="peer hidden" id="checkicon" />
+    <label htmlFor="checkicon" className="select-none p-3 rounded-md border-2 border-white cursor-pointer peer-checked:text-green-400">
+      Check me
+    </label>
+    <FieldError name={props.name} />
+  </div>
+))
+
 
