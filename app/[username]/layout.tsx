4630e564329b04b20b913aabe0ee66cc9f51eedb
Andrew Morrison <andrew.morrison@selfridges.com>
2023-08-12 10:50:48 +0100

    Add hambruger icon for sidebar toggle

--- app/[username]/layout.tsx
+++ app/[username]/layout.tsx
@@ -1,12 +1,14 @@
 import SideBar from "@/components/SideBar";
-
+import { Bars3Icon } from "@heroicons/react/20/solid"
 export default function UserDashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         <main className="flex-1 overflow-y-auto pt-8 px-6 ">
-          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+          <label htmlFor="my-drawer-2" className="btn lg:hidden">
+            <Bars3Icon className="w-10 h-10" />
+          </label>
           {children}
         </main>
       </div>
