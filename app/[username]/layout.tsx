b7d191258043c6b44f63201a829f27a6e4a2d04c
Andrew Morrison <andrew.morrisoon@gmail.com>
2023-08-29 17:19:34 +0100

    Add error boundary for login

--- app/[username]/layout.tsx
+++ app/[username]/layout.tsx
@@ -1,6 +1,8 @@
 import SideBar from "@/components/SideBar";
 import { Bars3Icon } from "@heroicons/react/20/solid"
-export default function UserDashboardLayout({ children }: { children: React.ReactNode }) {
+
+export default async function UserDashboardLayout({ children }: { children: React.ReactNode }) {
+
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -2,6 +2,7 @@ import { Form, useForm } from "@/components/form/form";
 import { Input } from "@/components/form/inputs";
 import { SupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import { useState, useTransition } from "react";
 import { SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 
@@ -18,22 +19,26 @@ const formSchema = z.object({
 export default function LoginForm({ router, supabase }: { router: AppRouterInstance, supabase: SupabaseClient }) {
 
   const form = useForm({ schema: formSchema })
+  const [isError, setIsError] = useState<Error>()
 
-  const onSubmit: SubmitHandler<LoginForm> = async (data) => {
+  if (isError) throw isError
+
+  const onSubmit: SubmitHandler<LoginForm> = async (formData) => {
     try {
-      const resp = await supabase.auth.signInWithPassword({
-        email: data.email,
-        password: data.password,
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: formData.email,
+        password: formData.password,
       })
       router.refresh()
 
-      if (resp.error) throw Error(resp.error.message)
-      if (resp.data) {
-        console.log('user logged in!', resp.data)
-        router.push(`/${resp.data.user?.user_metadata.username}/generate`)
+      if (error) {
+        throw (error.message)
+      }
+      if (data.user?.user_metadata.username) {
+        router.push(`/${data.user?.user_metadata.username}/generate`)
       }
     } catch (error) {
-      throw error
+      setIsError(new Error(`${error}`))
     }
   }
 
