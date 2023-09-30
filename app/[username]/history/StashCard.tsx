a713b54830ae6b77c36736b804e1db60642d0051
Andrew Morrison <41924973+AndyM10@users.noreply.github.com>
2023-09-30 23:12:22 +0100

    Show generation data on 'StashCards' in history page (#18)
    * hacky typescript fix
    
    * fix linting
    
    * Make use of Image component from next and get rid of empty values
    
    ---------
    
    Co-authored-by: Andrew Morrison <andrew.morrisoon@gmail.com>

--- app/[username]/history/StashCard.tsx
+++ app/[username]/history/StashCard.tsx
@@ -1,15 +1,20 @@
-import { Database } from "@/lib/database";
-import { object } from "zod";
-
-interface StashCardProps {
-  image: Database['public']['Tables']['images']['Row'],
+import { Database, Json } from "@/lib/database";
+import Image from "next/image";
+export interface StashCardProps {
+  imageData: {
+    race: string,
+    style: string,
+    role: string,
+    story: string,
+  },
+  imageDate: string,
   url: string
 }
 
 
-export default function StashCard({ image, url }: StashCardProps) {
-  const date = new Date(image.created_at)
-  const imageData = Object.keys(image.image_data || {})
+export default function StashCard({ imageDate, imageData, url }: StashCardProps) {
+  const date = new Date(imageDate)
+
   return (
     <div className="card w-full p-6 bg-base-200 shadow-xl mt-6">
       <div className="text-xl font-semibold">
@@ -17,14 +22,21 @@ export default function StashCard({ image, url }: StashCardProps) {
       </div>
       <div className="divider mt-2"></div>
       <div className="flex flex-col h-full w-full bg-base-100 ">
-        <img src={url} className="w-full h-full" />
+        <Image src={url} width={512} height={512} alt={imageData.race + imageData.role} />
         <div className="card-body">
-          {
-            imageData.map((data, index) => (
-              <div key={index} className="text-xl font-semibold">
-                : {data?.toString()}
-              </div>
-            ))}
+          <ul>
+            {
+              Object.entries(imageData).map(([key, value]) => {
+                if (value === "") return
+                return (
+                  <li key={key} className="list-disc font-semibold">
+                    {value}
+                  </li>
+                )
+              })
+            }
+
+          </ul>
         </div>
       </div>
     </div >
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'omanzfbebeorixnqltmy.supabase.co',
+        port: '',
+
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
