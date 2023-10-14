f92123f1d1c2204e0bc553a1d846f08017f97111
Andrew Morrison <andrew.morrisoon@gmail.com>
2023-10-14 23:15:39 +0100

    update styling

--- app/[username]/layout.tsx
+++ app/[username]/layout.tsx
@@ -21,9 +21,9 @@ export default async function UserDashboardLayout({ children }: { children: Reac
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
-        <main className="flex-1 overflow-y-auto pt-8 px-6 ">
-          <label htmlFor="my-drawer-2" className="btn lg:hidden">
-            <Bars3Icon className="w-10 h-10" />
+        <main className="flex-1 overflow-y-auto px-6 ">
+          <label htmlFor="my-drawer-2" className="btn my-2 lg:hidden">
+            <Bars3Icon className="w-5 h-5" />
           </label>
           {children}
         </main>
--- app/[username]/settings/page.tsx
+++ app/[username]/settings/page.tsx
@@ -4,7 +4,6 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center">
       <div>
-
         <h1 className="label text-3xl">Settings</h1>
       </div>
     </div>
