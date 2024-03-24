de6d8a066455468a7da89eaa46a6d7c4e4dfbf6f
Andrew Morrison <andrew.morrisoon@gmail.com>
2024-03-24 23:05:18 +0000

    add ssm params

--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -1,20 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as nodefunction from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as ssm from 'aws-cdk-lib/aws-ssm'
 import { Construct } from 'constructs';
 import path = require('path');
 
 export class DnDStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    console.log(process.env.OPENAI_API_KEY)
+
+    const OPENAI_API_KEY = ssm.StringParameter.fromStringParameterName(this, 'AI key', '/dndai/open_ai_key').stringValue
+    const SUPBASE_URL = ssm.StringParameter.fromStringParameterName(this, 'Supabase Url', '/dndai/supabase_url').stringValue
+    const SUPABASE_ANON_KEY = ssm.StringParameter.fromStringParameterName(this, 'Supbase Anon Key', '/dndai/supabase_anon_key').stringValue
 
     const generateFunction = new nodefunction.NodejsFunction(this, 'GenerateFunction', {
       entry: path.join(__dirname, '../../api/src/generate.ts'),
       timeout: cdk.Duration.seconds(20),
       environment: {
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY || '',
-        SUPBASE_URL: '',
-        SUPBASE_ANON_KEY: ''
+        OPENAI_API_KEY,
+        SUPBASE_URL,
+        SUPABASE_ANON_KEY
       }
     })
 
